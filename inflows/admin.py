a835c9d645bf2ca8f6169bf42aaf057f8fe8c7c8
DionisioAndrade <dionisio_andrade@outlook.com>
2026-03-23 13:49:43 -0300

    [feat] Admin register all apps

--- inflows/admin.py
+++ inflows/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Inflow
 
-# Register your models here.
+
+@admin.register(Inflow)
+class InflowAdmin(admin.ModelAdmin):
+    list_display = ['supplier', 'product', 'quantity',]
+    search_fields = ['supplier', 'product', 'quantity',]
